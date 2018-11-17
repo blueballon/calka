6d8acfed837052b93a580838015c2ef90b5baec4
Alexander Haas <Alexander Haas>
2018-11-17 16:39:48 +0100

    moved SWT-specific parts of App into SwtApp

--- CalkaSwt/src/main/java/org/blueballoon/calka/App.java
+++ CalkaSwt/src/main/java/org/blueballoon/calka/App.java
@@ -1,11 +1,6 @@
 package org.blueballoon.calka;
 
-import org.eclipse.swt.*;
-//import org.eclipse.swt.custom.*;
-//import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.widgets.*;
-import org.eclipse.swt.layout.*;
-import org.blueballoon.calka.calkaswt.SwtCalculatorButtons;
+import org.blueballoon.calka.calkaswt.CalkaSwtApp;
 
 /**
  * Hello world!
@@ -15,24 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Starting CalKa - the CALculator KAta (Java SWT Version)" );
-
-        Display calkaDisplay = new Display();
-        Shell calkaShell = new Shell(calkaDisplay);
-        calkaShell.setSize(500,500);
-        calkaShell.setText("CalKaSwt");
-        calkaShell.setLayout(new RowLayout());
-
-        final SwtCalculatorButtons calculatorButtons = new SwtCalculatorButtons(calkaShell, SWT.BORDER);
-
-
-
-        calkaShell.open();
-        while(!calkaShell.isDisposed()) {
-            if(!calkaDisplay.readAndDispatch()) {
-                calkaDisplay.sleep();
-            }
-        }
-        calkaDisplay.dispose();
+        System.out.println( "Starting CalKa - the CALculator KAta" );
+        CalkaSwtApp.Runner();
     }
 }
---
+++ CalkaSwt/src/main/java/org/blueballoon/calka/calkaswt/CalkaSwtApp.java
@@ -0,0 +1,32 @@
+package org.blueballoon.calka.calkaswt;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.*;
+import org.blueballoon.calka.calkaswt.SwtCalculatorButtons;
+
+public class CalkaSwtApp
+{
+    public static void Runner( )
+    {
+        System.out.println( "Running Calka Java/SWT Version" );
+
+        Display calkaDisplay = new Display();
+        Shell calkaShell = new Shell(calkaDisplay);
+        calkaShell.setSize(500,500);
+        calkaShell.setText("CalKaSwt");
+        calkaShell.setLayout(new RowLayout());
+
+        final SwtCalculatorButtons calculatorButtons = new SwtCalculatorButtons(calkaShell, SWT.BORDER);
+
+
+
+        calkaShell.open();
+        while(!calkaShell.isDisposed()) {
+            if(!calkaDisplay.readAndDispatch()) {
+                calkaDisplay.sleep();
+            }
+        }
+        calkaDisplay.dispose();
+    }
+}
